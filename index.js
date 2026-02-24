dff0b0cf2afbd3415837f04cbbe03166b870e48a
Hiroyuki-Komai <c84t2t6brd@privaterelay.appleid.com>
2026-02-24 19:54:16 +0900

    fix message

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.json({ message: "Hello CI/CD" });
+  res.json({ message: "Enthusiastic" });
 });
 
 module.exports = app;
